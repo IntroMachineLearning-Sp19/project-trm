b198646d1981dd353e5f8c779afdcc7ab830f758
Michael Barnard <michaeltherobot42@gmail.com>
2019-03-17 21:21:12 -0400

    Updated data structure

--- proj_part1.py
+++ proj_part1.py
@@ -3,13 +3,14 @@ Test fitting of team member data with different classifiers
 '''
 
 import os
+import glob
 import numpy as np
-import glob as glob
 from PIL import Image
 
 def load_images():
     '''
     Load all images as RGB data in an array
+    Classify all images
     '''
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,13 +18,24 @@ def load_images():
     letter_dirs.sort()
 
     image_list = []
+    image_class = []
     for curr_letter_dir in letter_dirs:
+        curr_letter = curr_letter_dir[-1] # Get last element of string
+        curr_letter.lower() # Make lowercase
+
         letter_image_list = []
+        letter_image_class = []
         for filename in glob.glob('%s/*' % (curr_letter_dir)):
             im = Image.open(filename)
-            imgRGB = list(im.getdata())    # a set of 4 values(R, G, B, A)
-            letter_image_list.append(imgRGB)
+            imgRGB = list(im.getdata()) # a set of 3 values(R, G, B)
+            letter_image_list.append(imgRGB) # Append RGB data list
+            letter_image_class.append(curr_letter) # Append classification
+
         image_list.append(letter_image_list)
+        image_class.append(letter_image_class)
+
+    image_arr = np.asarray(image_list)
+    image_class_arr = np.asarray(image_class)
 
     f = 1
 
