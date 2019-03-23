efd4805ba617bb8dac65047797edd713c315f3c4
Trung Tran <tdtran17@gmail.com>
2019-03-22 22:38:52 -0400

    cleanup help

---
+++ miscelaneous/README.md
@@ -0,0 +1,6 @@
+TEAM TRM
+
+PROJECT MEMBERS: TRUNG, NIKITA, ROSEMOND & MICHAEL
+
+
+[Data submission link](https://uflorida-my.sharepoint.com/:f:/g/personal/azare_ufl_edu/EkPEkKsG_OpOlgu2Cj0rQ0oBYvt1g5n__69MaATJINJ3LQ?e=KZ0BmB)
---
+++ miscelaneous/rgb_color_test.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 20:29:06 2019
+
+@author: rosem
+"""
+
+import os 
+
+if __name__ == "__main__":
+    os.system('proj_part1.py')
+
+    ########################## NO RED ###########################
+    red_0 = np.reshape(image_arr[0][0],(100,100,3))
+    for i in range(100):
+        for j in range(100):
+            red_0[i][j][0] = 0
+    plt.figure()
+    plt.title('Red 0')
+    plt.imshow(red_0)
+    
+    red_255 = np.reshape(image_arr[0][0],(100,100,3))
+    for i in range(100):
+        for j in range(100):
+            red_255[i][j][0] = 255
+    plt.figure()
+    plt.title('Red 255')
+    plt.imshow(red_255)
+    
+    ########################## NO GREEN ###########################
+    green_0 = np.reshape(image_arr[0][0],(100,100,3))
+    for i in range(100):
+        for j in range(100):
+            green_0[i][j][1] = 0
+    plt.figure()
+    plt.title('Green 0')
+    plt.imshow(green_0)
+    
+    green_255 = np.reshape(image_arr[0][0],(100,100,3))
+    for i in range(100):
+        for j in range(100):
+            green_255[i][j][1] = 255
+    plt.figure()
+    plt.title('Green 255')
+    plt.imshow(green_255)
+    
+    ########################## NO BLUE ###########################
+    blue_0 = np.reshape(image_arr[0][0],(100,100,3))
+    for i in range(100):
+        for j in range(100):
+            blue_0[i][j][2] = 0
+    plt.figure()
+    plt.title('Blue 0')
+    plt.imshow(blue_0)
+    
+    blue_255 = np.reshape(image_arr[0][0],(100,100,3))
+    for i in range(100):
+        for j in range(100):
+            blue_255[i][j][2] = 255
+    plt.figure()
+    plt.title('Blue 255')
+    plt.imshow(blue_255)
